c840e198622c3e78cf68fb55c485f85533a02c9b
Joseph Kinuthia <cjkinuthia5@gmail.com>
2024-04-03 12:48:58 +0300

    day 20/21 complete

---
+++ day20-snake_game/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+ # #detect collision with tail
+    # for segment in snake.segments[1:]:
+    #     if snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+    
