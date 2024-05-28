168d1098d340cc73ff277ce2e0e11b638025edf0
Joseph-kdev <cjkinuthia5@gmail.com>
2024-05-28 14:47:47 +0300

    slight changes

---
+++ day-24/main.py
@@ -0,0 +1,9 @@
+# file = open("my_file.txt")
+# content = file.read()
+# print(content)
+
+# with open("my_file.txt", mode="w") as file:
+#     file.write("Hello world")
+
+with open("/home/j/Documents/file.txt", mode="w") as file:
+    file.write("To be or not")
