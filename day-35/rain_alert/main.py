31508f7c84c62123488c76349ec1ef9344a9fa87
Joseph Kinuthia <cjkinuthia5@gmail.com>
2024-06-18 12:12:36 +0300

    day-35 completed, built an sms app

---
+++ day-35/rain_alert/main.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from twilio.rest import Client
+
+API = os.environ.get("OWM_API")
+MY_LAT = -1.167240
+MY_LONG = 36.825500
+
+account_sid ="AC799fae012fc2471f665648fe3ef4ea42"
+auth_token = os.environ.get("TWILIO_API")
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": API,
+    "exclude": "current,minutely,daily",
+}
+
+response = requests.get("https://api.openweathermap.org/data/3.0/onecall", params= parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+    
+    
+client = Client(account_sid, auth_token)
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15067993600',
+                     to='+254769067931'
+                 )
+                
+print(message.status)
